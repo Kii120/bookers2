aada6ba497b9eb42b9475bd451010f5e8b8c0e16
EC2 Default User <ec2-user@ip-172-31-4-33.ec2.internal>
2022-12-30 08:34:55 +0000

    [add] devise controller model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'books/new'
+  # get 'books/create'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  # get 'books/destroy'
+  # get 'homes/top'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  root to: "homes#top"
+  resources :users, only: [:show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
